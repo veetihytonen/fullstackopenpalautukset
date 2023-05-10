dbdaf54f0e14b19c888a44adfcc108cb092ba5af
Veeti Hytönen <veeti.hytonen@gmail.com>
2023-05-10 14:54:55 +0300

    did exercises for 'unicafe'

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const HeaderText = ({ text }) => (
+  <h1>
+    {text}
+  </h1>
+)
+
+const Button = ({ text, handleClick }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const StatisticLine = ({ text, value, postfix }) => (
+  <tr>
+    <td>
+      {text}
+    </td>
+    <td>
+      {value} {postfix}
+    </td>
+  </tr>
+)
+
+const Statistics = ({ good, neutral, bad }) => {
+
+  const all = good + neutral + bad
+
+  const average = ((good - bad) / (good + neutral + bad)).toFixed(1)
+
+  const positivePct = (good / (good + neutral + bad) * 100).toFixed(1)
+
+  if (good + neutral + bad === 0) {
+    return (
+      <>
+        No feedback given
+      </>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <table>
+      <tbody>
+        <StatisticLine text='good' value={good} />
+        <StatisticLine text='neutral' value={neutral} />
+        <StatisticLine text='bad' value={bad} />
+        <StatisticLine text={'all'} value={all} />
+        <StatisticLine text={'average'} value={average} />
+        <StatisticLine text={'positive'} value={positivePct} postfix={'%'} />
+      </tbody>
+    </table >
+  )
 }
 
-export default App;
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 })
+  const [nFeedback, setNFeedback] = useState(0)
+
+  const handleGood = () => {
+    setFeedback({ ...feedback, good: feedback.good + 1 })
+    setNFeedback(nFeedback + 1)
+  }
+  const handleNeutral = () => {
+    setFeedback({ ...feedback, neutral: feedback.neutral + 1 })
+    setNFeedback(nFeedback + 1)
+  }
+  const handleBad = () => {
+    setFeedback({ ...feedback, bad: feedback.bad + 1 })
+    setNFeedback(nFeedback + 1)
+  }
+
+  return (
+    <div>
+
+      <HeaderText text='give feedback'></HeaderText>
+
+      <div>
+        <Button handleClick={handleGood} text='good' />
+        <Button handleClick={handleNeutral} text='neutral' />
+        <Button handleClick={handleBad} text='bad' />
+      </div>
+
+      <HeaderText text='statistics'></HeaderText>
+
+      <div>
+        <Statistics
+          good={feedback.good}
+          neutral={feedback.neutral}
+          bad={feedback.bad} />
+      </div>
+
+    </div >
+  )
+}
+
+export default App
